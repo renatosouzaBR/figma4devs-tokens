30e1377e035e0eb7d53e724b3f8479df8a343e96
Renato Souza <natoicm@gmail.com>
2024-09-30 15:10:52 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray400": "#646363",
+  "ColorGray800": "#18181b",
+  "ColorGray900": "#09090b",
+  "ColorBlue500": "#0369a1"
+};
